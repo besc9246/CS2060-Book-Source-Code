b7d798a8f777d8263d168cd1c70ec42772d2f704
besc9246 <107508317+besc9246@users.noreply.github.com>
2022-06-21 23:07:03 -0600

    HW01
    Latest HW01

--- examples/ch02/fig02_01.c
+++ examples/ch02/fig02_01.c
@@ -1,59 +1,41 @@
 // Fig. 2.1: fig02_01.c
 // A first program in C 
 #include <stdio.h>
-#include <stdbool.h>
 
 // function main begins program execution 
 int main( void )
 {
-    const int goodScanfRtrtn = 1;
-    
-    //Create boolean flag for loop
-    bool cntnueLoop = true;
-
     unsigned int length = 0;
     unsigned int width = 0;
     unsigned int area = 0;
-    unsigned int scanfRtrn = -1;
-    
+    unsigned int divResult = 0;
+        
     puts("Enter the length.");
     
     //Read the length into the variable length.
-    scanfRtrn = scanf("%u", &length);
+    scanf("%u", &length);
     
-    //Clear buffer
-    while (getchar() != '\n');
+    puts("Enter the width.");
     
-    while (cntnueLoop) {
+    //Read the width into the variable width.
+    scanf("%u", &width);
     
-        if (scanfRtrn != goodScanfRtrtn) {
-            puts("Error using scanf for length.");
-            cntnueLoop = true;
-        }
-        
-        puts("Enter the width.");
-        
-        //Read the width into the variable width.
-        scanfRtrn = scanf("%u", &width);
-            
-        //Clear buffer
-        while (getchar() != '\n');
-        
-        if (scanfRtrn != goodScanfRtrtn) {
-            puts("Error using scanf for width.");
-            cntnueLoop = false;
-        }
-        
-        //Calculate the area and assign it to the area variable.
-        area = length * width;
-        
-        cntnueLoop = false;
-    }
+    //Calculate the area and assign it to the area variable.
+    area = length * width;
+    
     //Print out the information
     printf("Length = %u, Width = %u, and Area = %u.\n", length, width, area);
+        
+    divResult = length / width;
+        
+    printf("length / width = %u\n", divResult);
     
-    //printf( "Welcome to C!\n" );
-} // end function main 
+    //here is a syntax error
+    
+    return 0;
+    
+} // end function main
+
 
 
 /**************************************************************************
