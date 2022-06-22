33ba7cfcc650c628c4a60aee9e7c26c27e8111f3
besc9246 <107508317+besc9246@users.noreply.github.com>
2022-06-21 21:35:12 -0600

    fig02_01 with while loop
    fig02_01 with while loop

--- examples/ch02/fig02_01.c
+++ examples/ch02/fig02_01.c
@@ -1,11 +1,58 @@
 // Fig. 2.1: fig02_01.c
 // A first program in C 
 #include <stdio.h>
+#include <stdbool.h>
 
 // function main begins program execution 
 int main( void )
 {
-   printf( "Hello Ben. Welcome to C!\n" );
+    const int goodScanfRtrtn = 1;
+    
+    //Create boolean flag for loop
+    bool cntnueLoop = true;
+
+    unsigned int length = 0;
+    unsigned int width = 0;
+    unsigned int area = 0;
+    unsigned int scanfRtrn = -1;
+    
+    puts("Enter the length.");
+    
+    //Read the length into the variable length.
+    scanfRtrn = scanf("%u", &length);
+    
+    //Clear buffer
+    while (getchar() != '\n');
+    
+    while (cntnueLoop) {
+    
+        if (scanfRtrn != goodScanfRtrtn) {
+            puts("Error using scanf for length.");
+            cntnueLoop = true;
+        }
+        
+        puts("Enter the width.");
+        
+        //Read the width into the variable width.
+        scanfRtrn = scanf("%u", &width);
+            
+        //Clear buffer
+        while (getchar() != '\n');
+        
+        if (scanfRtrn != goodScanfRtrtn) {
+            puts("Error using scanf for width.");
+            cntnueLoop = false;
+        }
+        
+        //Calculate the area and assign it to the area variable.
+        area = length * width;
+        
+        cntnueLoop = false;
+    }
+    //Print out the information
+    printf("Length = %u, Width = %u, and Area = %u.\n", length, width, area);
+    
+    //printf( "Welcome to C!\n" );
 } // end function main 
 
 
