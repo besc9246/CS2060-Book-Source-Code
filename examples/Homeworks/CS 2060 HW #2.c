38e5aaa15f5acf74309043cf30e4b0f0e45c418c
besc9246 <107508317+besc9246@users.noreply.github.com>
2022-06-24 19:45:24 -0600

    Update to HW 02
    Update to HW 02

--- examples/Homeworks/CS 2060 HW #2.c
+++ examples/Homeworks/CS 2060 HW #2.c
@@ -10,37 +10,36 @@
 
 int main(void) {
     
-    //Vars
+    //Initialize vars
     double charge = 2.0;
     double chargeSum = 0.0;
     double numHours = 0.0;
     double totalHours = 0.0;
-    
+    double hrsCntr = 3.0;
     bool stopLoop = false;
-    
+    //bool validData = true;
     int scanfRtrn = -1;
-    const double min = 0;
-    const double max = 24.0;
     short carCnt = 0;
     
-    
-    double hrsCntr = 3.0;
-    
-    
+    //Constants
     const short sentinelVal = -1;
     const double maxHrsDaily = 18.0;
     const short scanfOkRtrnVal = 1;
+    const double min = 0.0;
+    const double max = 24.0;
 
     while (!stopLoop) {
         
         puts("Enter the number of hours the car was parked or enter -1 to quit.\n");
         
+        //Get user input
         scanfRtrn = scanf("%lf", &numHours);
         
+        //Clear buffer
         while (getchar() != '\n');
         
         if (scanfRtrn != scanfOkRtrnVal) {
-            puts("\nPlease enter a number.\n");
+            puts("\nPlease enter a number. No characters other than numbers and a decimal point are allowed.\n");
         }
         
         if (numHours != sentinelVal) {
@@ -48,44 +47,47 @@ int main(void) {
             if (scanfRtrn == scanfOkRtrnVal) {
             
                 if (numHours < min || numHours > max) {
-                    printf("\nInvalid input. Number of hours must be betwwen %.2lf and %.2lf\n\n", min, max);
+                    printf("\nInvalid input. Number of hours must be between %.2lf and %.2lf\n\n", min, max);
                 }
                 
-                if (numHours > sentinelVal && numHours <= 0) {
+               else if (numHours > sentinelVal && numHours < 0) {
                     puts("\nInvalid input.\n");
-                }
+               }
+            
+               else if (numHours == 0.0) {
+                    puts("\nThere were no cars parked today.\n");
+               }
                             
                 carCnt++;
                 
                 totalHours += numHours;
                 
-                if (!stopLoop) {
+                if (!stopLoop && numHours > min) {
                 
                     //Charge must be reset
-                    if (numHours <= 2.0) {
+                    if (numHours <= 2.0 && numHours > min) {
                         charge = 2.0;
                     }
-                    else {
+                    else if (numHours != min) {
                         charge = 1.5;
                     }
                     
                     //Hours counter must be reset
                     hrsCntr = 2.0;
+                    //hrsCntr = 3.0;
                     
-                    while (hrsCntr <= maxHrsDaily && hrsCntr < numHours) {
+                    while (hrsCntr <= maxHrsDaily && hrsCntr < numHours && numHours > min) {
                     
-                        if (numHours <= 3.0) {
+                        if (numHours <= 3.0 && numHours > min) {
                             charge = 2.0;
                         }
                         
                         else {
                             charge += 0.5;
                         }
-            //            if (numHours > (hrsCntr - 1.0) && numHours <= hrsCntr) {
-        //                    charge += 0.5;
-            //            }
-                        
+         
                         hrsCntr = hrsCntr + 1.0;
+                    
                     }//end while
                     
                     
